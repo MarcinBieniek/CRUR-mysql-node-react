240684415be2f9ad3b1c54058d4850f017f9cbc2
Marcin Bieniek <marcingim@gmail.com>
2023-01-11 23:24:03 +0100

    Fetch mysql data in client

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 const Books = () => {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        const fetchAllBooks = async () => {
+            try{
+                const res = await axios.get("http://localhost:8800/books")
+                setBooks(res.data)
+            } catch(err) {
+                console.log(err)
+            }
+        }
+        fetchAllBooks()
+    }, [])
+
     return (
-        <div>Books</div>
+        <div>
+            <h1>Marcin Book Shop</h1>
+            <div className="books">
+                {books.map((book) => (
+                    <div className="book" key={book.id}>
+                        {book.cover && <img src={book.cover} alt="" />}
+                        <h2>{book.title}</h2>
+                        <p>{book.dscr}</p>
+                        <span>{book.price}</span>
+                    </div>
+                ))}
+            </div>
+            <button>
+                <Link to="/add">Add new book</Link>
+            </button>
+        </div>
     )
 }
 
